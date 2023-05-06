84eee9f82510e4bcb555dc4fd7f8c8a9071cb794
shoxa-mir <shoxamiraliyev@gmail.com>
2023-05-06 12:22:10 +0900

    Start, pause, stop buttons functionality is added

--- main.py
+++ main.py
@@ -35,6 +35,9 @@ class VideoPlayer(QMainWindow):
         layout.addWidget(self.stop_button, 3, 0)
         self.stop_button.clicked.connect(self.start_capture)
 
+        self.fps_label = QLabel("FPS:", self)
+        layout.addWidget(self.fps_label, 3, 2)
+
         self.label1 = QLabel("Detection", self)
         layout.addWidget(self.label1, 0, 3, 1, 2)
 
@@ -58,53 +61,58 @@ class VideoPlayer(QMainWindow):
         self.central_widget.setLayout(layout)
         self.setCentralWidget(self.central_widget)
 
+        self.timer = QTimer(self)
+
         self.start_capture()
 
     def start_capture(self):
         self.empty_frame()
-        self.video_capture = cv2.VideoCapture(self.video)
+        self.ret = False
 
     def button_clicked(self):
-        self.timer = QTimer(self)
+        if not self.ret:
+            self.video_capture = cv2.VideoCapture(self.video)
         self.timer.timeout.connect(lambda: self.update_frame(self.video_capture))
 
         self.timer.start(1)
 
     def update_frame(self, vid_cap):
-        ret, frame = vid_cap.read()
+        self.ret, frame = vid_cap.read()
+        if self.ret == True:
+            try:
+                self.frame_count =+ 1
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                frame = cv2.resize(frame, (self.width, self.height))  
 
-        try:
-            self.frame_count =+ 1
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            frame = cv2.resize(frame, (self.width, self.height))  
+                draw_boxes, draw_segmentation, draw_all = self.infer(frame)
 
-            draw_boxes, draw_segmentation, draw_all = self.infer(frame)
+                draw_boxes = QImage(draw_boxes.data, draw_boxes.shape[1], draw_boxes.shape[0], QImage.Format_RGB888)
+                draw_boxes_pixmap = QPixmap.fromImage(draw_boxes)
 
-            draw_boxes = QImage(draw_boxes.data, draw_boxes.shape[1], draw_boxes.shape[0], QImage.Format_RGB888)
-            draw_boxes_pixmap = QPixmap.fromImage(draw_boxes)
+                draw_segmentation = QImage(draw_segmentation.data, draw_segmentation.shape[1], draw_segmentation.shape[0], QImage.Format_RGB888)
+                draw_segmentation_pixmap = QPixmap.fromImage(draw_segmentation)
 
-            draw_segmentation = QImage(draw_segmentation.data, draw_segmentation.shape[1], draw_segmentation.shape[0], QImage.Format_RGB888)
-            draw_segmentation_pixmap = QPixmap.fromImage(draw_segmentation)
+                draw_all = QImage(draw_all.data, draw_all.shape[1], draw_all.shape[0], QImage.Format_RGB888)
+                draw_all = QPixmap.fromImage(draw_all)
+                if self.frame_count % 10 == 0:
+                    time.sleep(10)
+                
+                self.video_label1.setPixmap(draw_boxes_pixmap)
+                self.video_label2.setPixmap(draw_segmentation_pixmap)
+                self.video_label3.setPixmap(draw_all)
 
-            draw_all = QImage(draw_all.data, draw_all.shape[1], draw_all.shape[0], QImage.Format_RGB888)
-            draw_all = QPixmap.fromImage(draw_all)
-            if self.frame_count % 10 == 0:
-                time.sleep(10)
-            
-            self.video_label1.setPixmap(draw_boxes_pixmap)
-            self.video_label2.setPixmap(draw_segmentation_pixmap)
-            self.video_label3.setPixmap(draw_all)
+            except:
+                if self.frame_count % 10 == 0:
+                    time.sleep(10)
+                self.empty_frame()
 
-        except:
-            if self.frame_count % 10 == 0:
-                time.sleep(10)
-            self.empty_frame()
+                self.counter += 1
+                if self.counter == 100:
+                    sys.exit(0)
 
-            self.counter += 1
-            if self.counter == 100:
-                sys.exit(0)
+            self.pause_button.clicked.connect(self.timer.stop)
+            self.stop_button.clicked.connect(self.timer.stop)
 
-        self.pause_button.clicked.connect(self.timer.stop)
 
     def initialize_model(self):
         # print("Model Initializing")
@@ -119,7 +127,8 @@ class VideoPlayer(QMainWindow):
         draw_segmentation = self.roadEstimator.draw_segmentation(frame)
         draw_boxes = self.roadEstimator.draw_boxes(frame)
         draw_all = self.roadEstimator.draw_2D(frame)
-        print(f"{1 / (time.time() - t1)} fps")
+        # print(f"{1 / (time.time() - t1)} fps")
+        self.fps_label.setText(f"FPS: {round(1 / (time.time() - t1), 2)}")
         return draw_boxes, draw_segmentation, draw_all
 
     def empty_frame(self):
